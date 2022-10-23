0a33fbb9b71437a23e9551479328755812473dad
Carlin Nicola <nicola.carlin-1@studenti.unitn.it>
2022-10-23 11:20:03 +0200

    Add put method

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -6,18 +6,14 @@ const Event = mongoose.model('Event',model);
 
 let MSG = {
     eventNotFound: "Event not found",
-    updateFailed: "Event update failed"
+    updateFailed: "Event update failed",
+    createFailed: "Event create failed"
 }
 
 module.exports.get_events = (req, res) => {};
 
 module.exports.create_event = (req, res) => {
-    let uuid = req.para
     
-    
-    Event.create({
-
-    })
 };
 
 module.exports.delete_events = (req, res) => {};
@@ -40,7 +36,41 @@ module.exports.get_event = (req, res) => {
 };
 
 module.exports.update_event = (req, res) => {
-
+    let uuid = req.params.uuid;
+    let title = req.body.title;
+    let category = req.body.category
+    let description = req.body.description
+    let dates = req.body.dates
+    let joinOptions = req.body.joinOptions
+    let website = req.body.website
+    let physicalAddress = req.body.physicalAddress
+    let contacts = req.body.contacts
+    
+    Event.findOneAndUpdate({
+            "uuid": uuid
+        }, {
+            "title": title,
+            "category": category,
+            "description": description,
+            "dates": dates,
+            "joinOptions": joinOptions,
+            "website": website,
+            "physicalAddress": physicalAddress,
+            "contacts": contacts
+        }, {
+            new:true
+        },
+        (err, event) => {
+            if (err) {
+                res.status(400).json({
+                    error: MSG.updateFailed
+                })
+            }
+            else {
+                res.status(200).json(event);
+            }
+        }
+    )
 };
 
 module.exports.delete_event = (req, res) => {
