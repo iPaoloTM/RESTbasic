58d3c327f726d95f88c36ac787a455f9fbbbc3e4
Carlin Nicola <nicola.carlin-1@studenti.unitn.it>
2022-10-30 20:37:52 +0100

    Try (not working) get mobilizon events

--- index.js
+++ index.js
@@ -16,3 +16,43 @@ config.initDB()
     .catch(err => {
         throw(new Error(err));
     });
+
+    const GraphQLClient = require('graphql-request').GraphQLClient
+
+    const client = new GraphQLClient('http://localhost', {
+      headers: {
+        //Authorization: 'Bearer YOUR_AUTH_TOKEN',
+        email: "test@mail.com",
+        password: "mypassword"
+      },
+    });
+    
+    
+    client.request(`
+        {
+          events{
+            elements{
+              uuid
+               title
+              category
+              description
+              endsOn
+              beginsOn
+              joinOptions
+              onlineAddress
+              physicalAddress{
+                geom
+                country
+                region
+                locality
+                street
+                postalCode
+              }
+              contacts{
+                name
+                summary
+              }
+            }
+          }
+        }
+      `).then(events => console.log(events)).catch(err => console.log(err))
