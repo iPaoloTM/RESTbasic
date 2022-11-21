cbf35bf4e91ba8e7f76f2b2e850f3b4445ec6b1f
Simone <simone.compri@gmail.com>
2022-11-21 22:56:36 +0100

    events.js different from events - solution.js

--- app/routes/apiV1/events.js
+++ app/routes/apiV1/events.js
@@ -9,10 +9,4 @@ router.get('/', eventHandler.get_events);
 
 router.post('/', eventHandler.create_event);
 
-router.get('/:uuid', eventHandler.get_event);
-
-router.delete('/:uuid', eventHandler.delete_event);
-
-router.put('/:uuid', eventHandler.update_event);
-
 module.exports = router;
